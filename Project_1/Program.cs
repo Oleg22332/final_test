2c0ee2e3aefb1651f615289f22df861487e40c8f
Oleg Suroegin <suroegin.oleg.07122004@gmail.com>
2024-06-04 11:46:54 +0300

    add program

---
+++ Project_1/Program.cs
@@ -0,0 +1,29 @@
+﻿//Создание первого массива
+string[] Array = {"1234", "1567", "-2", "computer science"};
+//Нахождение длинны нового массива
+int count = 0;
+for (int i = 0; i < Array.Length; i++)
+{
+    if (Array[i].Length <= 3)
+    {
+        count++;
+    }
+}
+//Создание нового массива
+string[] NewArray = new string [count];
+//Заполнение нового массива
+int j = 0;
+for (int i = 0; i < Array.Length; i++)
+{
+    if (Array[i].Length <= 3)
+    {
+        NewArray[j] = Array[i];
+        j++;
+    }
+}
+//Печать нового массива
+for (j = 0; j < NewArray.Length; j++)
+{
+    System.Console.WriteLine(NewArray[j]);
+
+}
